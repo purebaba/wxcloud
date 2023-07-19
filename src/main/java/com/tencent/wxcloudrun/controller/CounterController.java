679bd978d5d5b1bf4eae29b8c51cabefcf35a31c
purebaba <huangdekai@gmail.com>
2023-07-19 18:00:00 +0800

    feat: test wxdata

--- src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
@@ -10,6 +10,9 @@ import com.tencent.wxcloudrun.service.CounterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.Optional;
 
 /**
@@ -19,49 +22,59 @@ import java.util.Optional;
 @Slf4j
 public class CounterController {
 
-  final CounterService counterService;
-  final Logger logger;
+    final CounterService counterService;
+    final Logger logger;
 
-  public CounterController(@Autowired CounterService counterService) {
-    this.counterService = counterService;
-    this.logger = LoggerFactory.getLogger(CounterController.class);
-  }
-
-
-  /**
-   * 获取当前计数
-   * @return API response json
-   */
-  @GetMapping(value = "/api/count")
-  ApiResponse get(@RequestParam(required = false) String signature, @RequestParam(required = false) String timestamp, @RequestParam(required = false) String nonce, @RequestParam(required = false) String echostr) {
-    logger.info("/api/count get request");
-    log.info("signature:{}", signature);
-    log.info("timestamp:{}", timestamp);
-    log.info("nonce:{}", nonce);
-    log.info("echostr:{}", echostr);
-    return ApiResponse.ok();
-  }
-
-  @GetMapping("/api/wx-data")
-  public ApiResponse getWxData(@RequestParam(required = false) String signature, @RequestParam(required = false) String timestamp, @RequestParam(required = false) String nonce, @RequestParam(required = false) String echostr) {
-    log.info("signature:{}", signature);
-    log.info("timestamp:{}", timestamp);
-    log.info("nonce:{}", nonce);
-    log.info("echostr:{}", echostr);
-    return ApiResponse.ok();
-  }
+    public CounterController(@Autowired CounterService counterService) {
+        this.counterService = counterService;
+        this.logger = LoggerFactory.getLogger(CounterController.class);
+    }
 
 
+    @GetMapping({"/api/wx-data", "/api/count"})
+    public ApiResponse getWxData(HttpServletRequest request, @RequestParam(required = false) String signature, @RequestParam(required = false) String timestamp, @RequestParam(required = false) String nonce, @RequestParam(required = false) String echostr) {
+        // 详细打印request重的信息，包括请求路径，body等
+        log.info("request:{}", request);
+        log.info("request path:{}", request.getPathInfo());
+        log.info("request query string:{}", request.getQueryString());
+        log.info("signature:{}", signature);
+        log.info("timestamp:{}", timestamp);
+        log.info("nonce:{}", nonce);
+        log.info("echostr:{}", echostr);
+        return ApiResponse.ok();
+    }
 
-  /**
-   * 更新计数，自增或者清零
-   * @param request {@link CounterRequest}
-   * @return API response json
-   */
-  @PostMapping(value = "/api/count")
-  ApiResponse create(@RequestBody Object request) {
-    logger.info("/api/count post request, action: {}", request.toString());
-    return ApiResponse.ok();
-  }
-  
+
+    /**
+     * 更新计数，自增或者清零
+     *
+     * @param request {@link HttpServletRequest}
+     * @return API response json
+     */
+    @PostMapping(value = "/api/count")
+    ApiResponse create(HttpServletRequest request) {
+        logger.info("/api/count post request, action: {}", request.toString());
+        log.info("request:{}", request);
+        log.info("request path:{}", request.getPathInfo());
+        log.info("request query string:{}", request.getQueryString());
+        log.info("request query string:{}", handleRequest(request));
+        return ApiResponse.ok();
+    }
+
+
+    private String handleRequest(HttpServletRequest request) {
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader reader = request.getReader()) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 现在sb中包含了请求的body数据，您可以进行相应的处理。
+        return "Received data: " + sb.toString();
+    }
+
 }
